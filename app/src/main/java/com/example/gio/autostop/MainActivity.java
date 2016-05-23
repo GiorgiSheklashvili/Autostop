5d651306d3f7d27ec23926ffa7b8ca5d631540d9
Giorgi Sheklashvili <sheklashviligiorgi@gmail.com>
2016-05-23 16:36:35 +0400

    moving Location update to MapsActivity

--- app/src/main/java/com/example/gio/autostop/MainActivity.java
+++ app/src/main/java/com/example/gio/autostop/MainActivity.java
@@ -1,75 +1,20 @@
 package com.example.gio.autostop;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import com.google.android.gms.location.LocationListener;
-
-import android.location.LocationManager;
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderApi;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+public class MainActivity extends AppCompatActivity {
         public Button button1;
-    public GoogleApiClient mGoogleApiClient;
-    public LocationRequest locationRequest;
-    public int permissionRequestCounter =0;
-    public FusedLocationProviderApi LocationProvider = LocationServices.FusedLocationApi;
-    public final static int MILISECONDS_PER_SECOND = 1000;
-    public final static int REQUEST_FINE_LOCATION = 0;
-    public final static int MINUTE = 60 * MILISECONDS_PER_SECOND;
-    public Boolean mRequestingLocationUpdates;
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates)
-//            requestLocationUpdates();
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRequestingLocationUpdates=false;
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(MINUTE);
-        locationRequest.setFastestInterval(15 * MILISECONDS_PER_SECOND);
-        locationRequest.setPriority(com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY);
         button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
                                        @Override
@@ -84,10 +29,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void displayMap(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
-        //        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
-//                == PackageManager.PERMISSION_GRANTED)
-//        else
-//            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
     }
 
     @Override
@@ -97,14 +38,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         return true;
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopLocationUpdates();
-    }
-    protected void stopLocationUpdates(){
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -121,71 +54,4 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         return super.onOptionsItemSelected(item);
     }
 
-    private void requestLocation(){
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, MainActivity.this);
-            } else
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
-        }
-    }
-    @Override
-    public void onConnected(Bundle bundle) {
-//        requestLocationUpdates();
-    }
-
-
-    private void requestLocationUpdates() {
-            // Should we show an explanation?
-            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                Toast.makeText(MainActivity.this, "Location access is required to display your location", Toast.LENGTH_SHORT).show();
-                // aq shemodis tu motxovnaze uari tqva momxmarebelma magram never ask me again ar monishna
-                if(permissionRequestCounter==0){//amis gareshe meorejer da shemdeg motxovnebze tu momxmarebeli
-                // never ask me again-is gareshe monishnavda deny-s motxovna icikleboda
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
-                    permissionRequestCounter++;
-                }
-            } else {
-                // aq shemodis pirvelad motxovnisas da aseve motxovnis uaryopis shemtvevashi tu momxmarebelma tan never ask again monishna
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
-            }
-            mRequestingLocationUpdates=true;
-
-
-    }
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//
-//        switch (requestCode) {
-//            case REQUEST_FINE_LOCATION: {
-//                if (grantResults.length==1
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    requestLocation();
-//
-//                } else {
-//                    Toast.makeText(MainActivity.this, "Permission was blocked", Toast.LENGTH_SHORT).show();
-//
-//                }
-//            }
-//        }
-//
-//    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-//        Toast.makeText(this,"Location Changed; "+location.getLatitude()+" "+location.getLongitude(),Toast.LENGTH_SHORT).show();
-
-    }
 }
--- app/src/main/java/com/example/gio/autostop/MapsActivity.java
+++ app/src/main/java/com/example/gio/autostop/MapsActivity.java
@@ -2,27 +2,34 @@ package com.example.gio.autostop;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import com.google.android.gms.location.LocationListener;
 import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
-import java.util.Map;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMyLocationButtonClickListener{
+public class MapsActivity extends FragmentActivity implements LocationListener,GoogleApiClient.OnConnectionFailedListener,OnMapReadyCallback,GoogleMap.OnMyLocationButtonClickListener,GoogleApiClient.ConnectionCallbacks{
 
     private GoogleMap mMap;// Might be null if Google Play services APK is not available.
-    public final static int REQUEST_FINE_LOCATION = 0;
     public int permissionRequestCounter =0;
+    public GoogleApiClient mGoogleApiClient;
+    public Boolean mRequestingLocationUpdates;
+    public LocationRequest locationRequest;
+    public final static int MILISECONDS_PER_SECOND = 1000;
+    public final static int REQUEST_FINE_LOCATION = 0;
+    public final static int MINUTE = 60 * MILISECONDS_PER_SECOND;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +39,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mRequestingLocationUpdates=false;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(MINUTE);
+        locationRequest.setFastestInterval(15 * MILISECONDS_PER_SECOND);
+        locationRequest.setPriority(com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+//        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates)
+//            requestLocationUpdates();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if(mRequestingLocationUpdates)
+        requestLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
     }
 
     private void setUpMapIfNeeded() {
@@ -52,6 +97,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+    protected void stopLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected())
+        stopLocationUpdates();
+
+    }
+
+    private void requestLocationUpdates() {
+        // Should we show an explanation?
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(MapsActivity.this, "Location access is required to display your location", Toast.LENGTH_SHORT).show();
+            // aq shemodis tu motxovnaze uari tqva momxmarebelma magram never ask me again ar monishna
+            if(permissionRequestCounter==0){//amis gareshe meorejer da shemdeg motxovnebze tu momxmarebeli
+                // never ask me again-is gareshe monishnavda deny-s motxovna icikleboda
+
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+                permissionRequestCounter++;
+            }
+        } else {
+            // aq shemodis pirvelad motxovnisas da aseve motxovnis uaryopis shemtvevashi tu momxmarebelma tan never ask again monishna
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+        }
+        mRequestingLocationUpdates=true;
+
+
+    }
+
+    private void requestLocation(){
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, MapsActivity.this);
+            } else
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -60,16 +146,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         enableMyLocation();
     }
     private void enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission to access the location is missing.
-            if(permissionRequestCounter==0) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
-                permissionRequestCounter++;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission to access the location is missing.
+                if (permissionRequestCounter == 0) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+                    permissionRequestCounter++;
+                }
+            } else if (mMap != null) {
+                // Access to the location has been granted to the app.
+                mMap.setMyLocationEnabled(true);
             }
-        } else if (mMap != null) {
-            // Access to the location has been granted to the app.
-            mMap.setMyLocationEnabled(true);
         }
     }
     @Override
@@ -83,21 +171,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 } else {
                     Toast.makeText(MapsActivity.this, "Permission was blocked", Toast.LENGTH_SHORT).show();
-
                 }
             }
         }
 
     }
-
     @Override
     public boolean onMyLocationButtonClick() {
         Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
         return false;
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
     private void setUpMap() {
-//        mMap.addMarker(new MarkerOptions().position(new LatLng(41.705239, 44.776229)).title("My Marker"));
     }
 
 }
