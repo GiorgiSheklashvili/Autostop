2ad9365af664cf4856c658664d2e6555efbf46d0
gio <sheklashviligiorgi@gmail.com>
2016-12-10 03:02:47 +0400

    Animation Fourth try,Almost Done

--- app/src/main/java/com/example/gio/autostop/TrapezoidDrawableView.java
+++ app/src/main/java/com/example/gio/autostop/TrapezoidDrawableView.java
@@ -16,8 +16,9 @@ import android.view.animation.Transformation;
 
 
 public class TrapezoidDrawableView extends View {
-    Paint white, gray, black;
+    Paint white, gray;
     int whiteInt;
+    int k = 22;
     Point[] leftTop = new Point[3];
     Point[] rightTop = new Point[3];
     Point[] leftBottom = new Point[3];
@@ -25,6 +26,8 @@ public class TrapezoidDrawableView extends View {
     Path trapezoid;
     boolean oneThird = false;
     boolean twoThird = false;
+    boolean twoThirdForWhite = false;
+    boolean widthGainOn = false;
     Path miniTrapezoid0, miniTrapezoid1, miniTrapezoid2;
     int width;
     double height;
@@ -51,18 +54,13 @@ public class TrapezoidDrawableView extends View {
         white.setStyle(Paint.Style.FILL);
         white.setColor(whiteInt);
 
-
-        black = new Paint();
-        black.setAntiAlias(true);
-        black.setStyle(Paint.Style.FILL);
-        black.setColor(ContextCompat.getColor(getContext(), R.color.black));
-
         gray = new Paint();
         gray.setAntiAlias(true);
         gray.setColor(Color.GRAY);
         gray.setStyle(Paint.Style.FILL_AND_STROKE);
         gray.setStrokeWidth(10);
         gray.setAntiAlias(true);
+        displayWidthandHeight();
         for (int i = 0; i < 3; i++) {
             leftTop[i] = new Point();
             leftBottom[i] = new Point();
@@ -79,6 +77,15 @@ public class TrapezoidDrawableView extends View {
         startAnimation(animation);
     }
 
+    public void displayWidthandHeight() {
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = (size.y * 1.0) / 3.0;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -89,19 +96,22 @@ public class TrapezoidDrawableView extends View {
         trapezoid.lineTo(canvas.getWidth() / 20, canvas.getHeight());
         trapezoid.lineTo(canvas.getWidth() / 3, 0);
         canvas.drawPath(trapezoid, gray);
-        drawMiniTrapezoid(canvas, miniTrapezoid0, 0);
-        canvas.drawPath(miniTrapezoid0, white);
-        if (reachedOneThird(leftTop[0].y)) {
+        if (twoThirdForWhite || reachedOneThird(leftTop[2].y)) {
+            twoThirdForWhite = true;
+            drawMiniTrapezoid(canvas, miniTrapezoid0, 0);
+            canvas.drawPath(miniTrapezoid0, white);
+        }
+        if (oneThird || reachedOneThird(leftTop[0].y)) {
             oneThird = true;
             drawMiniTrapezoid(canvas, miniTrapezoid1, 1);
-            canvas.drawPath(miniTrapezoid1, black);
+            canvas.drawPath(miniTrapezoid1, white);
         }
-//        if (reachedTwoThird(leftTop[0].y) && reachedOneThird(leftTop[1].y)) {
-//            twoThird = true;
-//            drawMiniTrapezoid(canvas, miniTrapezoid2, 2);
-//            canvas.drawPath(miniTrapezoid2, white);
-//        }
+        if (twoThird || reachedOneThird(leftTop[1].y)) {
+            twoThird = true;
 
+            drawMiniTrapezoid(canvas, miniTrapezoid2, 2);
+            canvas.drawPath(miniTrapezoid2, white);
+        }
 
 
     }
@@ -114,6 +124,8 @@ public class TrapezoidDrawableView extends View {
         path.lineTo(canvas.getWidth() * 42 / 81 + rightBottom[i].x, (canvas.getHeight() / 5) + rightBottom[i].y);
         path.lineTo(canvas.getWidth() * 39 / 81 - leftBottom[i].x, (canvas.getHeight() / 5) + leftBottom[i].y);
         path.lineTo(canvas.getWidth() * 40 / 81 - leftTop[i].x, leftTop[i].y);
+
+
     }
 
     public boolean reachedOneThird(double y) {
@@ -122,11 +134,6 @@ public class TrapezoidDrawableView extends View {
         return false;
     }
 
-    public boolean reachedTwoThird(double y) {
-        if (y >= height * 3.0 / 4.0)
-            return true;
-        return false;
-    }
 
     private class ScrollingAnimation extends Animation {
         public ScrollingAnimation() {
@@ -152,59 +159,69 @@ public class TrapezoidDrawableView extends View {
         @Override
         protected void applyTransformation(float interpolatedTime, Transformation t) {
             super.applyTransformation(interpolatedTime, t);
-            displayWidthandHeight();// initshia gasatani  tu initidan shemovedi aq ar vamateb arapers
-            leftTop[0].x = leftTop[0].x + 1;
-            leftTop[0].y = leftTop[0].y + 10;
-            rightTop[0].x = rightTop[0].x + 1;
-            rightTop[0].y = rightTop[0].y + 10;
-            rightBottom[0].x = rightBottom[0].x + 1;
-            rightBottom[0].y = rightBottom[0].y + 10;
-            leftBottom[0].x = leftBottom[0].x + 1;
-            leftBottom[0].y = leftBottom[0].y + 10;
+            if (widthGainOn) {
+                leftTop[0].x = leftTop[0].x + 1;
+                rightTop[0].x = rightTop[0].x + 1;
+                rightBottom[0].x = rightBottom[0].x + 1;
+                leftBottom[0].x = leftBottom[0].x + 1;
+            }
+            leftTop[0].y = leftTop[0].y + 5;
+            rightTop[0].y = rightTop[0].y + 5;
+            rightBottom[0].y = rightBottom[0].y + 5;
+            leftBottom[0].y = leftBottom[0].y + 5;
             if (oneThird) {
-                leftTop[1].x = leftTop[1].x + 1;
-                leftTop[1].y = leftTop[1].y + 10;
-                rightTop[1].x = rightTop[1].x + 1;
-                rightTop[1].y = rightTop[1].y + 10;
-                rightBottom[1].x = rightBottom[1].x + 1;
-                rightBottom[1].y = rightBottom[1].y + 10;
-                leftBottom[1].x = leftBottom[1].x + 1;
-                leftBottom[1].y = leftBottom[1].y + 10;
+                if (widthGainOn) {
+                    leftTop[1].x = leftTop[1].x + 1;
+                    rightTop[1].x = rightTop[1].x + 1;
+                    rightBottom[1].x = rightBottom[1].x + 1;
+                    leftBottom[1].x = leftBottom[1].x + 1;
+                }
+
+                leftTop[1].y = leftTop[1].y + 5;
+                rightTop[1].y = rightTop[1].y + 5;
+                rightBottom[1].y = rightBottom[1].y + 5;
+                leftBottom[1].y = leftBottom[1].y + 5;
             }
             if (twoThird) {
-                leftTop[2].x = leftTop[2].x + 1;
-                leftTop[2].y = leftTop[2].y + 10;
-                rightTop[2].x = rightTop[2].x + 1;
-                rightTop[2].y = rightTop[2].y + 10;
-                rightBottom[2].x = rightBottom[2].x + 1;
-                rightBottom[2].y = rightBottom[2].y + 10;
-                leftBottom[2].x = leftBottom[2].x + 1;
-                leftBottom[2].y = leftBottom[2].y + 10;
+                if (widthGainOn) {
+                    leftTop[2].x = leftTop[2].x + 1;
+                    rightTop[2].x = rightTop[2].x + 1;
+                    rightBottom[2].x = rightBottom[2].x + 1;
+                    leftBottom[2].x = leftBottom[2].x + 1;
+                }
+
+                leftTop[2].y = leftTop[2].y + 5;
+                rightTop[2].y = rightTop[2].y + 5;
+                rightBottom[2].y = rightBottom[2].y + 5;
+                leftBottom[2].y = leftBottom[2].y + 5;
             }
 
-            if (checkingIfSurpassed(leftTop[0].y))
+            if (checkingIfSurpassed(leftTop[0].y)) {
+                twoThirdForWhite = false;
                 makeZero(0);
+            }
 
-            if (checkingIfSurpassed(leftTop[1].y))
+            if (checkingIfSurpassed(leftTop[1].y)) {
+                oneThird = false;
                 makeZero(1);
-//            if (checkingIfSurpassed(leftTop[2].y))
-//                makeZero(2);
+            }
+            if (checkingIfSurpassed(leftTop[2].y)) {
+                twoThird = false;
+                makeZero(2);
+            }
+            if (widthGainOn)
+                widthGainOn = false;
+            else
+                widthGainOn = true;
+
             TrapezoidDrawableView.this.requestLayout();
             TrapezoidDrawableView.this.invalidate();
+
         }
 
         public boolean checkingIfSurpassed(int Ycoordinate) {
 
             return Ycoordinate >= (int) height;
-        }
-
-        public void displayWidthandHeight() {
-            WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-            Display display = wm.getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            width = size.x;
-            height = (size.y * 1.0) / 3.0;
         }
 
 
